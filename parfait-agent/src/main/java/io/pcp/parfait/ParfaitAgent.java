6f765e3c498610dbea271c6f9cf31560f7b76905
PratyushTalreja <pratyush.talreja@gmail.com>
2017-06-13 02:19:54 +0530

    Created a new POJO class Specification and altered some code

--- parfait-agent/src/main/java/io/pcp/parfait/ParfaitAgent.java
+++ parfait-agent/src/main/java/io/pcp/parfait/ParfaitAgent.java
@@ -1,6 +1,7 @@
 package io.pcp.parfait;
 
 import io.pcp.parfait.MonitoringViewProperties;
+import io.pcp.parfait.dxm.PcpMmvWriter;
 import io.pcp.parfait.jmx.MonitoredMBeanAttributeFactory;
 import io.pcp.parfait.DynamicMonitoringView;
 
@@ -8,12 +9,8 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 import java.net.ConnectException;
-import java.util.Collection;
 
 import org.apache.log4j.Logger;
-import org.json.JSONObject;
-import org.springframework.beans.BeansException;
-import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -43,23 +40,23 @@ public class ParfaitAgent {
         }
     }
 
-    private static void loadContextProfile(GenericXmlApplicationContext context, String profile) {
-        context.getEnvironment().setActiveProfiles(profile);
-        context.load("classpath:agent.xml");
-        try {
-            context.load("file:/etc/parfait/*.xml");
-        } catch (Exception e) {
-            logger.trace("Cannot setup beans from /etc/parfait", e);
-        } 
-        context.refresh();
-    }
-
     public static void startLocal() {
-        DynamicMonitoringView view = null;
+    	Specification spec[] = null;
+        DynamicMonitoringView view;
+        PcpMmvWriter pMmvWriter;
+        MonitorableRegistry mRegistry = new MonitorableRegistry();
+        MonitoredMBeanAttributeFactory<?> mMBeanAttributeFactory = null;
         try {
-        	parseJSON(view);
-        } catch (BeansException e) {
-            logger.error("Stopping Parfait agent, cannot setup beans", e);
+        	parseJSON(spec);
+        	for (int i = 0; i < spec.length; i++)
+        	{
+        		mMBeanAttributeFactory = new MonitoredMBeanAttributeFactory<>(spec[i].getName(), spec[i].getDescription(), spec[i].getmBeanName(), spec[i].getmBeanAttributeName(), spec[i].getmBeanCompositeDataItem());
+		        mMBeanAttributeFactory.setMonitorableRegistry(mRegistry);
+        	}
+        	view = new DynamicMonitoringView((MonitoringView) mMBeanAttributeFactory);
+			view.start();
+        } catch (Exception e) {
+            logger.error(e);
         }
     }
 
@@ -80,27 +77,22 @@ public class ParfaitAgent {
     }
 
     public static void startProxy(String jmx) {
-        DynamicMonitoringView view;
-        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
-
+        DynamicMonitoringView view = null;
+        
         try {
-            loadContextProfile(context, "proxy");
-            view = (DynamicMonitoringView)context.getBean("monitoringView");
             view.start();
             Thread.currentThread().join();    // pause the main proxy thread
         } catch (Exception e) {
             String m = "Stopping Parfait proxy";  // pretty-print some errors
             if (getCause(e) instanceof ConnectException) {
                 logger.error(String.format("%s, cannot connect to %s", m, jmx));
-            } else if (e instanceof BeansException) {
+            } else if (e instanceof /*BeansException*/ Exception) {
                 logger.error(String.format("%s, cannot setup beans", m), e);
             } else if (e instanceof InterruptedException) {
                 logger.error(String.format("%s, interrupted", m));
             } else {
                 logger.error(m, e);
             }
-        } finally {
-            context.close();
         }
     }
 
@@ -112,12 +104,10 @@ public class ParfaitAgent {
         }
     }
     
-    public static void parseJSON(DynamicMonitoringView view)
+	public static void parseJSON(Specification spec[])
     {
     	ObjectMapper mapper = new ObjectMapper();
-    	String result = "";
-    	MonitorableRegistry mRegistry = new MonitorableRegistry();
-    	MonitoringView mView = null;
+    	int counter = 0;
 		try {
 			JsonNode root = mapper.readTree(new File("/home/pratyush/Desktop/jvm.config"));
 			JsonNode metrics = root.path("metrics");
@@ -128,19 +118,15 @@ public class ParfaitAgent {
 				String mBeanName = node.path("mBeanName").asText();
 				String mBeanAttributeName = node.path("mBeanAttributeName").asText();
 				String mBeanCompositeDataItem = node.path("mBeanCompositeDataItem").asText();
-				result += name + "\n" + description + "\n" + units + "\n" + mBeanName + "\n" + mBeanAttributeName + "\n" + mBeanCompositeDataItem + "\n";
-				/*System.out.println("Name : " + name);
-				System.out.println("Description : " + description);
-				System.out.println("Units : " + units);
-				System.out.println("mBeanName : " + mBeanName);
-				System.out.println("mBeanAttributeName : " + mBeanAttributeName);
-				System.out.println("mBeanCompositeDataItem : " + mBeanCompositeDataItem);
-				System.out.println("--------------------------------------------------");*/
-		        MonitoredMBeanAttributeFactory<?> mMBeanAttributeFactory = new MonitoredMBeanAttributeFactory<>(name, description, mBeanName, mBeanAttributeName, mBeanCompositeDataItem);
-		        mMBeanAttributeFactory.setMonitorableRegistry(mRegistry);
+				spec[counter] = new Specification();
+				spec[counter].setName(name);
+				spec[counter].setDescription(description);
+				spec[counter].setUnits(units);
+				spec[counter].setmBeanName(mBeanName);
+				spec[counter].setmBeanAttributeName(mBeanAttributeName);
+				spec[counter].setmBeanCompositeDataItem(mBeanCompositeDataItem);
+				counter++;
 			}
-			view = new DynamicMonitoringView(mRegistry, mView);
-			view.start();
 		} catch (JsonGenerationException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
---
+++ parfait-agent/src/main/java/io/pcp/parfait/Specification.java
@@ -0,0 +1,54 @@
+package io.pcp.parfait;
+
+public class Specification {
+	String name, description, units, mBeanName, mBeanAttributeName, mBeanCompositeDataItem;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUnits() {
+		return units;
+	}
+
+	public void setUnits(String units) {
+		this.units = units;
+	}
+
+	public String getmBeanName() {
+		return mBeanName;
+	}
+
+	public void setmBeanName(String mBeanName) {
+		this.mBeanName = mBeanName;
+	}
+
+	public String getmBeanAttributeName() {
+		return mBeanAttributeName;
+	}
+
+	public void setmBeanAttributeName(String mBeanAttributeName) {
+		this.mBeanAttributeName = mBeanAttributeName;
+	}
+
+	public String getmBeanCompositeDataItem() {
+		return mBeanCompositeDataItem;
+	}
+
+	public void setmBeanCompositeDataItem(String mBeanCompositeDataItem) {
+		this.mBeanCompositeDataItem = mBeanCompositeDataItem;
+	}
+	
+}
